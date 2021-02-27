87d3410300b689fed545f01ec25124193a05b565
Benson <tumlivein@gmail.com>
2021-02-27 23:08:57 +0800

    Querying Data with MongoDB

--- server/schema/schema.ts
+++ server/schema/schema.ts
@@ -4,26 +4,10 @@ import {
     GraphQLString,
     GraphQLInt,
     GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull,
 } from 'graphql'
 import Product from '../models/product'
-
-// dummy data
-const products = [
-    {
-        id: '1',
-        name: '極緻活顏煥肌活顏素',
-        type: '精華液',
-        rating: 5,
-        brand: 'The Body Shop',
-    },
-    {
-        id: '2',
-        name: '特潤超導全方位修護露',
-        type: '精華液',
-        rating: 5,
-        brand: 'Estee Lauder',
-    },
-]
 
 const ProductType = new GraphQLObjectType({
     name: 'Product',
@@ -39,13 +23,19 @@ const ProductType = new GraphQLObjectType({
 const Query = new GraphQLObjectType({
     name: 'Query',
     fields: {
+        products: {
+            type: new GraphQLList(ProductType),
+            resolve(parent, args) {
+                return Product.find({})
+            },
+        },
         product: {
             type: ProductType,
             args: {
-                id: { type: GraphQLID },
+                id: { type: new GraphQLNonNull(GraphQLID) },
             },
             resolve(parent, args) {
-                return products.find((product) => product.id === args.id)
+                return Product.findById(args.id)
             },
         },
     },
@@ -57,10 +47,10 @@ const Mutation = new GraphQLObjectType({
         addProduct: {
             type: ProductType,
             args: {
-                name: { type: GraphQLString },
-                type: { type: GraphQLString },
-                rating: { type: GraphQLInt },
-                brand: { type: GraphQLString },
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                type: { type: new GraphQLNonNull(GraphQLString) },
+                rating: { type: new GraphQLNonNull(GraphQLInt) },
+                brand: { type: new GraphQLNonNull(GraphQLString) },
             },
             resolve(parent, args) {
                 const product = new Product({ ...args })
