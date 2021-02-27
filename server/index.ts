4481acb9cf12875cd270c4e1197ae0b82e983b21
Benson <tumlivein@gmail.com>
2021-02-27 20:57:03 +0800

    Connecting to MongoDB

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect(
+    'mongodb+srv://Benson:YueGp60208@cluster0.zjfsh.mongodb.net/cosmetics?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+mongoose.connection.once('open', () => console.log('Connected to MongoDB'))
 
 app.listen(4000, () =>
     console.log('Server is running on http://localhost:4000')
